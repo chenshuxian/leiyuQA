45c9494ca1ad511cfb2dc1c786c6c5ebe4403d0a
zioncheng <zioncheng71@gmail.com>
2021-10-04 01:20:36 +0800

    fix: fix the `Already 10 Prisma Clients are actively running` error

---
+++ libs/prisma.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client'
+
+// fix the `Already 10 Prisma Clients are actively running` error
+// https://flaviocopes.com/nextjs-fix-prismaclient-unable-run-browser/
+
+let prisma
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
+
+export default prisma
