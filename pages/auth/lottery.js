c08197017372e7fecc321cc94fe977ac363e1fe8
chenshuxian <j1092333120@gmail.com>
2021-10-19 14:28:48 +0800

    lottery 前端完成

--- pages/auth/lottery.js
+++ pages/auth/lottery.js
@@ -1,10 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 import Layout from '../../components/layout'
 import Image from 'next/image'
 import mainBanner from '../../public/assets/images/mainBanner.png';
 import { signIn, signOut, useSession } from 'next-auth/client'
 import DatePicker from 'react-datepicker';
 import { useState } from 'react';
-import { Form,DropdownButton, Dropdown, Container, Row, Col, Button } from 'react-bootstrap';
+import { Form, DropdownButton, Dropdown, Container, Row, Col, Button } from 'react-bootstrap';
 import "../../node_modules/react-datepicker/dist/react-datepicker.min.css"
 import { registerLocale, setDefaultLocale } from 'react-datepicker';
 import zh_TW  from '../../node_modules/date-fns/locale/zh-TW'
@@ -16,12 +18,30 @@ const prisma = new PrismaClient()
 
 function Lottery ({ prizeList, prizeObj }) {
     const date = new Date()
+    const lotteryList = ['陳書賢','吳書賢','吳小賢','的書賢','二小賢','吳臣賢','吳小靈']
     const [startDate, setStartDate] = useState(date)
     const [endDate, setEndDate] =useState(date)
-    const [dropTitle, setDropTitle] = useState('獎品清單')
+    const [prizeId, setPrizeId] = useState(Object.keys(prizeObj)[0])
+    const [luckyName, setLuckyName] = useState("準備中")
+    
 
-    const prizeSelect = (v) => {
-      setDropTitle(prizeObj[v])
+    const prizeSelect = (e) => {
+      console.log(e.target.value)
+      //setDropTitle(prizeObj[e.target.value])
+      setPrizeId(e.target.value)
+    }
+
+    const drawer = () => {
+      //console.log("drawer")
+      const randomName = () => {
+        console.log("drawer1")
+        let len = lotteryList.length
+        let luckyNum = Math.floor(Math.random() * len)
+        setLuckyName(lotteryList[luckyNum])
+      }
+
+      let timeoutID = setInterval(() => { randomName()},100)
+      setTimeout(()=>{clearInterval(timeoutID)},5000)
     }
 
   return (
@@ -41,83 +61,61 @@ function Lottery ({ prizeList, prizeObj }) {
                                 <h2>摸彩頁</h2>
                             </div>
                             <div className="globalContent"> 
-                                <Container className="lottery">
-                                    <Row>
-                                      <Col xs={12} md={3}>
-                                      <Row> 
-                                            <div className="lable">
-                                            獎品清單:
-                                            </div>
-                                      <DropdownButton
-                                        title={dropTitle}
-                                        id="dropdown-menu-align-right"
-                                        onSelect={prizeSelect}
-                                          >
-                                            {prizeList.map((v,i) => (
-                                              <div key={i}>
-                                              <Dropdown.Item eventKey={v.prize_id}>{v.prize_name}</Dropdown.Item>
-                                              </div>
-                                            ))}
-                                        </DropdownButton>
-          
-                                        </Row>
-                                      </Col>
-                                      <Col xs={12} md={3}>
-                                
-                                          <div className="lable">
-                                          開始日期:
-                                          </div>
-                                          <DatePicker selected={startDate} onChange={(date) => setStartDate(date)}
+                              <Form inline style={{justifyContent:"space-around"}}>
+                                <Form.Row xs={12} md={3}>
+                                  <Form.Group as={Col}  controlId="prizeList">
+                                    <Form.Label>獎品清單: </Form.Label>
+                                    <Form.Control as="select" onChange={prizeSelect} defaultValue={prizeId}>
+                                      {prizeList.map((v,i) => (
+                                          <option key={`prizeList${i}`} value={v.prize_id}>{v.prize_name}</option>
+                                      ))}
+                                    </Form.Control>
+                                  </Form.Group>
+                                </Form.Row>
+                                <Form.Row xs={12} md={3}>
+                                  <Form.Group as={Col}  controlId="startDate">
+                                    <Form.Label>開始日期: </Form.Label>
+                                    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)}
                                             locale="TW"
                                             withPortal
                                             portalId="root-portal"
                                             dateFormat="yyyy/MM"
                                             showMonthYearPicker   />
-                                        
-                                      </Col>
-                                      <Col xs={12} md={3}>
-                                        <Row> 
-                                          <div className="lable">
-                                          結束日期:
-                                          </div>
-                                            <DatePicker selected={endDate} onChange={(date) => setEndDate(date)}
+                                  </Form.Group>
+                                </Form.Row>
+                                <Form.Row xs={12} md={3}>
+                                  <Form.Group as={Col}  controlId="endDate">
+                                    <Form.Label>結束日期: </Form.Label>
+                                    <DatePicker selected={endDate} onChange={(date) => setEndDate(date)}
                                               locale="TW"
                                               withPortal
                                               portalId="root-portal"
                                               dateFormat="yyyy/MM"
                                               showMonthYearPicker
                                                  />
-                                            </Row>
-                                        </Col>
-                                        <Col xs={12} md={3}>
-                                          <Row> 
-                                            <div className="lable">
-                                            抽獎數量:
-                                            </div>
-                                            <Form.Control width="2" value="1" />
-                                            </Row>
-                                        </Col>
-                                    </Row>
-                                </Container>
-                                {/* <h6 className="number"><DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />摸彩次數 <b>9</b></h6> */}
-                                
-                                {/* <ul className="input">
-                                    <li><b>結束日期</b><input type="text" className="datetimepicker" /></li>
-                                </ul>
-                                <div className="name">
-                                    <ul>
-                                        <li>陳小東</li>
-                                        <li>陳小東</li>
-                                        <li>陳小東</li>
-                                        <li>陳小東</li>
-                                        <li>陳小東</li>
-                                        <li>陳小東</li>
-                                        <li>陳小東</li>
-                                        <li>陳小東</li>
-                                        <li>陳小東</li>
-                                    </ul>
-                                </div> 
-  <input type="submit" value="摸彩去" /> */}
+                                  </Form.Group>
+                                </Form.Row>
+                                <Form.Row xs={12} md={3}>
+                                  <Form.Group as={Col}  controlId="endDate">
+                                    <Form.Label>抽獎人數: </Form.Label>
+                                    <Form.Control type="text" defaultValue="1"></Form.Control>
+                                  </Form.Group>
+                                </Form.Row>
+                              </Form>
+                              <h6 className="number" style={{borderBottom: "4px solid #d8d8d8"}}></h6>
+                              <Container>
+                                  <Row style={{justifyContent:"center",alignItems:"center",textAlign:"center"}}>
+                                    <Col xs={12} md={6} >
+                                      <img src={`/assets/images/${prizeObj[prizeId]}`} className="lotteryImg" />
+                                    </Col>
+                                    <Col xs={12} md={6} >
+                                        <h1>{luckyName}</h1>
+                                    </Col>
+                                  </Row>
+                                  <Row style={{justifyContent:"center",alignItems:"center",textAlign:"center"}}>
+                                    <Button style={{width:"50%"}} onClick={drawer}>摸彩</Button>
+                                  </Row>
+                              </Container>
                             </div>
                         </div>
                     </dd>
@@ -135,11 +133,12 @@ export async function getStaticProps(context) {
   const prizeList = await prisma.prize.findMany({
       select:{
           prize_id:true,
-          prize_name:true
+          prize_name:true,
+          prize_image_url:true
       }
   })
 
-  const prizeObj = prizeList.reduce((obj, cur) => ({...obj, [cur.prize_id]: cur.prize_name}), {})
+  const prizeObj = prizeList.reduce((obj, cur) => ({...obj, [cur.prize_id]: cur.prize_image_url}), {})
 
   return {
     props: {
