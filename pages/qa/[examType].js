c7d2954960b63fddbe82399f9ad51553fdbb1b9e
chenshuxian <j1092333120@gmail.com>
2022-08-27 13:00:06 +0800

    fix mobile fb share bug

--- pages/qa/[examType].js
+++ pages/qa/[examType].js
@@ -18,10 +18,10 @@ function check() {
  var userAgentInfo=navigator.userAgent;
  var Agents =new Array("Android","iPhone","SymbianOS","Windows Phone","iPad","iPod");
  console.log(`agent : ${userAgentInfo}`)
- var flag=true;
+ var flag=false;
  for(var v=0;v<Agents.length;v++) {
    if(userAgentInfo.indexOf(Agents[v])>0) {
-    flag=false;
+    flag=true;
     break;
    }
   }
